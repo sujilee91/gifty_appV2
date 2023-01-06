3ea5efb96a20fbda02bfa16b9dac378ab5d35dad
Su Ji Lee <sujilee91@gmail.com>
2023-01-05 22:04:03 -0700

    fixxxxx

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../../img/fullLogo.png'
+
+import { Sidebar, HeaderWrapper, Button } from './styles'
+const SidebarComponent = () => {
+  return (
+    <Sidebar>
+      <Image src={logo} width={80} />
+      <HeaderWrapper>
+        <ul>
+          <li>
+            <Link href={'/groups'}>Groups</Link>
+          </li>
+          <li>
+            <Link href={'/wishlist'}>Wishlist</Link>
+          </li>
+          <li>
+            <Link href={'/friends'}>Friends</Link>
+          </li>
+          <li>
+            <Link href={'/profile'}>Profile</Link>
+          </li>
+        </ul>
+        {/* <h3>Hi, {currentUser?.name}!</h3> */}
+        <Button onClick={() => onLogout()} primary>
+          Logout
+        </Button>
+      </HeaderWrapper>
+    </Sidebar>
+  )
+}
+
+export default SidebarComponent
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,8 @@
-import Link from 'next/link'
-import Image from 'next/image'
-
 import App from '../components/App'
-import '../styles/global.css'
-import { Sidebar, HeaderWrapper, Button } from '../styles/styles'
-
-import logo from '../img/fullLogo.png'
+import Sidebar from '../components/Sidebar'
 const Main = () => {
   return (
     <div>
-      <Sidebar>
-        <Image src={logo} width={80} />
-        <HeaderWrapper>
-          <ul>
-            <li>
-              <Link href={'/groups'}>Groups</Link>
-            </li>
-            <li>
-              <Link href={'/wishlist'}>Wishlist</Link>
-            </li>
-            <li>
-              <Link href={'/friends'}>Friends</Link>
-            </li>
-            <li>
-              <Link href={'/profile'}>Profile</Link>
-            </li>
-          </ul>
-          {/* <h3>Hi, {currentUser?.name}!</h3> */}
-          <Button onClick={() => onLogout()} primary>
-            Logout
-          </Button>
-        </HeaderWrapper>
-      </Sidebar>
       <App />
     </div>
   )
